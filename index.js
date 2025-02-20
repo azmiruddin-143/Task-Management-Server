eed7438a5d38a90c4ef258e8752e570a596f15ea
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2025-02-21 03:25:44 +0600

    delete method complete task

--- index.js
+++ index.js
@@ -70,6 +70,13 @@ async function run() {
     })
 
 
+    app.delete("/task/:id",async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result)
+    })
+
     // await client.db("admin").command({ ping: 1 });
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
